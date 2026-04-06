7517a79435025f8cf603e5c1807cbf449b94bfe0
JackIzumihara <1551500700@k12.hi.us>
2026-04-06 21:58:36 +0000

    Working on fixing upg 2

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 let score = 0;
 let multi = 0;
-let upgrade1Amount = 0;
-let upgrade1Cost = 50;
-let upgrade2Amount = 0;
+let furnaceUpgAmount = 0;
+let furnaceUpgCost = 50;
+let pickaxeUpgAmount = 0;
+let pickaxeUpgCost = 250;
 let upgrade3Amount = 0;
+let pointsPerSec = 0;
 
 let multiDisplay = document.getElementById('scoreMulti');
 console.log(multiDisplay);
 
+let ppsDisplay = document.getElementById('pointsPerSec');
+
 let scoreDisplay = document.getElementById('scoreDisplay');
 console.log(scoreDisplay);
 
-let upg1CostDisplay = document.getElementById('upgrade1');
-let upg1AmountDisplay = document.getElementById('upgrade1Amount');
+let furnaceCostDisplay = document.getElementById('upgrade1');
+let furnaceAmountDisplay = document.getElementById('upgrade1Amount');
+
+let pickaxeCostDisplay = document.getElementById('upgrade2');
+let pickaxeAmountDisplay = document.getElementById('upgrade2Amount');
 
 function clickedButton(){
     score = score + multi + 1;
@@ -20,27 +27,49 @@ function clickedButton(){
     console.log(score);
 };
 
+setInterval(pointPerSecAdder(), 1000)
+
+function pointPerSecAdder(){
+    score = score + pointsPerSec;
+    scoreDisplay.innerHTML = "Score = " + score;
+}
+
+function devMode(){
+    score = score + 1000000000000;
+    scoreDisplay.innerHTML = "Score = " + 1000000000000;
+}
+
 function upgrade1(){
-    if(score >= (upgrade1Cost)){
-        score = score - upgrade1Cost;
+    if(score >= (furnaceUpgCost)){
+        score = score - furnaceUpgCost;
         multi = multi + 1;
-        upgrade1Amount++;
-        if (upgrade1Cost <=1000){
-            upgrade1Cost = (upgrade1Cost * 1.25);
-        }else if(upgrade1Cost >=1000 && upgrade1Cost <=5000){
-            upgrade1Cost = (upgrade1Cost * 1.1);
-        }else if(upgrade1Cost >=5000){
-            upgrade1Cost = (upgrade1Cost * 1.005 * (upgrade1Amount * 0.001));
+        furnaceUpgAmount++;
+        if (furnaceUpgCost <=1000){
+            furnaceUpgCost = (furnaceUpgCost * 1.25);
+        }else if(furnaceUpgCost >=1000 && furnaceUpgCost <=5000){
+            furnaceUpgCost = (furnaceUpgCost * 1.1);
+        }else if(furnaceUpgCost >=5000){
+            furnaceUpgCost = (furnaceUpgCost * 1.005 * (furnaceUpgAmount * 0.001));
         }
-        upgrade1Cost = Math.floor(upgrade1Cost);
+        furnaceUpgCost = Math.floor(furnaceUpgCost);
         scoreDisplay.innerHTML = "Score = " + score;
-        upg1CostDisplay.innerHTML = "+ 1 per upgrade - $" + upgrade1Cost;
-        upg1AmountDisplay.innerHTML = upgrade1Amount + " [upgradename] owned";
-        console.log("upgrade1 was used successfully");
+        furnaceCostDisplay.innerHTML = "+ 1 per click - $" + furnaceUpgCost;
+        furnaceAmountDisplay.innerHTML = furnaceUpgAmount + " Furnace upgrade(s) owned";
         console.log("Score is now " + score);
-        console.log("You now have " + upgrade1Amount + " upgrade1s");
-        console.log("Upgrade 1 now costs " + upgrade1Cost);
+        console.log("You now have " + furnaceUpgAmount + " Furnace upgrade(s)");
+        console.log("Upgrade 1 now costs " + furnaceUpgCost);
+        multiDisplay.innerHTML = "Points Per Click: " + (multi + 1);
     }else{
         console.log("not enough money or issue in code");
+    }
+}
+
+function upgrade2(){
+    if(score >= (pickaxeUpgCost)){
+        pointsPerSec++;
+        score = score - pickaxeUpgCost;
+        alert(pointsPerSec);
+    }else{
+        alert("Not enough money!");
     }
 }
